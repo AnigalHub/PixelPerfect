05a8c9c0388955ebac7f16f75bb68e388259241c
AnigalHub <anigal1997@gmail.com>
2021-05-10 19:01:43 +0300

    добавлен самый главный - первый компонент

---
+++ src/components/Home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="Home">
+        <div id="background">
+            <Navbar/>
+            <b-container>
+                <p id="Welcome_to_STARTUP">WELCOME TO STARTUP</p>
+                <div id="Your_Favourite_Creative_Age">Your Favourite Creative Agency Template</div>
+                <div id="Get_Started">Get Started</div>
+            </b-container>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Navbar from "./navbar";
+    export default {
+        name: "Home",
+        components: {Navbar}
+    }
+</script>
+
+<style scoped lang="scss">
+#Home{
+    background: url("../../public/Home.png") 100% 100% no-repeat;
+    background-size:100% 100%;
+    height: 770px;
+    #background{
+        padding-top: 20px;
+        background-color: rgba(0, 0, 0, 0.48);
+        height: 100%;
+    }
+    #Welcome_to_STARTUP{
+        font-size: 47.8px;
+        font-family: "Montserrat";
+        color: rgb(255, 255, 255);
+        text-transform: uppercase;
+        line-height: 0.5;
+        margin-top: 248px;
+        margin-bottom: 2.55rem !important;
+        font-weight: 600;
+        text-align: center;
+        z-index: 434;
+    }
+    #Your_Favourite_Creative_Age {
+        font-size: 19px;
+        font-family: "Roboto Slab";
+        color: rgb(255, 255, 255);
+        line-height: 0.5;
+        letter-spacing: 0.65px;
+        text-align: center;
+        z-index: 434;
+    }
+    #Get_Started {
+        font-size: 16px;
+        font-family: "Montserrat";
+        color: rgb(255, 255, 255);
+        text-transform: uppercase;
+        line-height: 1.5;
+        font-weight: 500;
+        width: 1164px;
+        margin-top: 70px;
+        text-align: center;
+        z-index: 428;
+    }
+
+
+
+
+}
+</style>
