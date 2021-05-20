065995d023d0647451732054c718c236ad22e0a3
AnigalHub <anigal1997@gmail.com>
2021-05-20 21:39:44 +0300

    добавлены картинки

---
+++ src/components/Clients.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="Clients">
+        <b-container>
+           <b-row>
+               <b-col>
+                   <img src="../../public/clients5.png">
+               </b-col>
+               <b-col>
+                   <img src="../../public/clients4.png">
+               </b-col>
+               <b-col>
+                   <img src="../../public/clients1.png">
+               </b-col>
+               <b-col>
+                   <img src="../../public/clients3.png">
+               </b-col>
+               <b-col>
+                   <img src="../../public/clients2.png">
+               </b-col>
+           </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Clients"
+    }
+</script>
+
+<style scoped>
+    #Clients{
+        padding-top: 105px;
+        padding-bottom: 121px;
+        background: #f8f8f8;
+    }
+    .row{
+        padding: 0 90px !important;
+    }
+    .col{
+        padding: 0 0 0 30px !important;
+    }
+</style>
