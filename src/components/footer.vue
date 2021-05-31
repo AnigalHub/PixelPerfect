997ff4f0e455c6cdd66b871449cac0fbe33cd0ee
AnigalHub <anigal1997@gmail.com>
2021-05-31 23:14:43 +0300

    компонент footer

---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="footer">
+р
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+#footer{
+    background-color: rgb(192, 48, 28);
+    height: 35vh;
+}
+</style>
