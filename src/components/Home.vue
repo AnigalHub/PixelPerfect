658fcbd74a49fc440105c0f14bb198351eaaba8d
AnigalHub <anigal1997@gmail.com>
2021-06-05 21:31:55 +0300

    убраны стили в общий файл, переделан регистр фразы

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
         <div id="background">
             <Navbar/>
             <b-container>
-                <p id="Welcome_to_STARTUP">WELCOME TO STARTUP</p>
+                <p id="Welcome_to_STARTUP">Welcome to StartUp</p>
                 <div id="Your_Favourite_Creative_Age">Your Favourite Creative Agency Template</div>
                 <div id="Get_Started">Get Started</div>
             </b-container>
@@ -20,51 +20,5 @@
 </script>
 
 <style scoped lang="scss">
-#Home{
-    background: url("../../public/Home.png") 100% 100% no-repeat;
-    background-size:100% 100%;
-    height: 770px;
-    #background{
-        padding-top: 20px;
-        background-color: rgba(0, 0, 0, 0.45);
-        height: 100%;
-    }
-    #Welcome_to_STARTUP{
-        margin-top: 249.5px;
-        font-size: 47.8px;
-        font-family: 'Montserrat', sans-serif;
-        color: rgb(255, 255, 255);
-        text-transform: uppercase;
-        line-height: 0.5;
-        margin-bottom: 2.55rem !important;
-        font-weight: 600;
-        text-align: center;
-    }
-    #Your_Favourite_Creative_Age {
-        font-size: 19px;
-        font-family: 'Roboto Slab', serif;
-        color: rgb(255, 255, 255);
-        line-height: 0.5;
-        letter-spacing: 0.65px;
-        text-align: center;
-    }
-    #Get_Started {
-        font-size: 16px;
-        font-family: 'Montserrat', sans-serif;
-        color: rgb(255, 255, 255);
-        text-transform: uppercase;
-        line-height: 1.5;
-        font-weight: 500;
-        text-align: center;
-        border: 2px solid white;
-        width: 180px;
-        padding: 1% ;
-        display: block;
-        margin: 57px auto 0 41.5%;
-    }
 
-
-
-
-}
 </style>
