d313551ace1847b05e52060bd42e2f1fe129e0c1
AnigalHub <anigal1997@gmail.com>
2021-06-05 21:29:58 +0300

    добавлены стили на Home и Service

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,6 @@ export default {
     About_us,
     Services,
     Home
-
   }
 }
 </script>
@@ -64,6 +63,117 @@ export default {
   display: flex;
   flex-wrap: wrap;
 }
-/*параметры сетки бутстрап*/
+$shadeOfWhite:rgb(255, 255, 255);
+$shadeOfGrey:rgb(85, 85, 85);
+
+#Home{
+  background: url("../public/Home.png") 100% 100% no-repeat;
+  background-size:100% 100%;
+  height: 770px;
+  color: $shadeOfWhite;
+  #background{
+    padding-top: 20px;
+    background-color: rgba(0, 0, 0, 0.45);
+    height: 100%;
+  }
+  #Welcome_to_STARTUP{
+    margin-top: 249.5px;
+    font-size: 47.8px;
+    margin-bottom: 2.55rem;
+    font-weight: 600;
+    text-align: center;
+  }
+  #Your_Favourite_Creative_Age {
+    font-size: 19px;
+    font-family: 'Roboto Slab', serif;
+    letter-spacing: 0.65px;
+  }
+  #Get_Started {
+    font-size: 16px;
+    font-weight: 500;
+    border: 2px solid $shadeOfWhite;
+    width: 180px;
+    padding: 1% ;
+    margin: 57px auto 0 41.5%;
+  }
+  #Welcome_to_STARTUP,  #Your_Favourite_Creative_Age{line-height: 0.5;}
+  #Welcome_to_STARTUP, #Your_Favourite_Creative_Age, #Get_Started{text-align: center;}
+  #Welcome_to_STARTUP,#Get_Started{
+    font-family: 'Montserrat', sans-serif;
+    text-transform: uppercase;
+  }
+}
+.name_component  {
+  font-size: 19.5px;
+  font-family: 'Montserrat', sans-serif;
+  color: rgb(85, 85, 85);
+  font-weight: 600;
+  text-transform: uppercase;
+  line-height: 1.2;
+  text-align: center;
+}
+.descr_component{
+  font-size: 15.5px;
+  font-family: 'Roboto Slab', serif;
+  color: rgb(85, 85, 85);
+  letter-spacing: .28px;
+  line-height: 1.5;
+  text-align: center;
+  margin-top: 24px;
+}
+.underline{
+  background-color: rgb(85, 85, 85);
+  margin-top: 25px;
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+  width: 50px;
+  height: 2px;
+}
+#Services{
+  padding-top: 95px;
+  padding-bottom: 95px;
+  text-align: center;
+  color: $shadeOfGrey;
+  .flex-container{
+    margin-top: 68px;
+    & > div {
+      width: 30%;
+      height: 100%;
+      margin: 0 1.4% 0 1.82% !important;
+    }
+  }
+  .name_services{
+    font-size: 13.9px;
+    font-family: 'Montserrat', sans-serif;
+    margin-top: 24px;
+    letter-spacing: -.1px;
+    margin-bottom: 0.9rem !important;
+    font-weight: 600;
+    text-transform: uppercase;
+
+  }
+  .descr_services{
+    font-size: 13.55px;
+    letter-spacing: 0.28px;
+    font-weight: 400;
+    font-family: 'Roboto Slab', serif;
+    line-height: 1.8;
+  }
+  .svg_services{
+    width: 93px;
+    height: 91px;
+    display: block;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+
+}
+@media (min-width: 1200px){
+  #Services{
+    .container, .container-sm, .container-md, .container-lg, .container-xl {max-width: 1200px !important;}
+  }
+}
 
 </style>
