b518cd10859789483f1ef762cc24080815abceaf
AnigalHub <anigal1997@gmail.com>
2021-06-09 20:58:23 +0300

    убраны стили - перенесены в App.vue

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,7 +2,7 @@
     <div id="Blog">
         <b-container>
             <div class="name_component">RECENT BLOG POSTS</div>
-            <svg class="underline" xmlns="http://www.w3.org/2000/svg" width="50px" height="2px">
+            <svg class="underline" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd"  fill="rgb(85, 85, 85)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999 "/>
             </svg>
             <p class="descr_component">Lorem ipsum dolor sit amet, consetetur sadipscing elitr amet</p>
@@ -13,29 +13,17 @@
                 <b-col class="text part_2">
                    <b-row>
                        <b-col cols="2" >
-                           <div class="date">
-                               18
-                               <br>
-                               <div class="month">
-                                   ост
-                               </div>
+                           <div class="date">18<br>
+                               <div class="month">ост</div>
                            </div>
                        </b-col>
                        <b-col>
-                           <div class="name_post">
-                               STARTUP IDEAS NEEDS TO BE FUNDED
-                           </div>
-                           <div>
-                               By Khalil Uddin in Development
-                           </div>
+                           <div class="name_post">STARTUP IDEAS NEEDS TO BE FUNDED</div>
+                           <div>By Khalil Uddin in Development</div>
                        </b-col>
                    </b-row>
-                    <div class="text">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod teduntlabore et dolore magna aliquyam erat, sed diam voluptua.  At vero eos et accusam et justo duo dolores et erebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit am Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidulabore et dolore aliquyam erat, sed diam.
-                    </div>
-                    <div class="read">
-                        READ MORE
-                    </div>
+                    <div class="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod teduntlabore et dolore magna aliquyam erat, sed diam voluptua.  At vero eos et accusam et justo duo dolores et erebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit am Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidulabore et dolore aliquyam erat, sed diam.</div>
+                    <div class="read">READ MORE</div>
                 </b-col>
             </b-row>
             <svg xmlns="http://www.w3.org/2000/svg" width="1140px" height="1px">
@@ -48,29 +36,17 @@
                 <b-col class="text part_2">
                     <b-row>
                         <b-col cols="2" >
-                            <div class="date">
-                                18
-                                <br>
-                                <div class="month">
-                                    ост
-                                </div>
+                            <div class="date">18<br>
+                                <div class="month">ост</div>
                             </div>
                         </b-col>
                         <b-col>
-                            <div class="name_post">
-                               USER INTERFACE DESIGNING ELEMENTS
-                            </div>
-                            <div>
-                                By Khalil Uddin in Development
-                            </div>
+                            <div class="name_post">USER INTERFACE DESIGNING ELEMENTS</div>
+                            <div>By Khalil Uddin in Development</div>
                         </b-col>
                     </b-row>
-                    <div class="text">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod teduntlabore et dolore magna aliquyam erat, sed diam voluptua.  At vero eos et accusam et justo duo dolores et erebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit am Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidulabore et dolore aliquyam erat, sed diam.
-                    </div>
-                    <div class="read">
-                        READ MORE
-                    </div>
+                    <div class="text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod teduntlabore et dolore magna aliquyam erat, sed diam voluptua.  At vero eos et accusam et justo duo dolores et erebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit am Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidulabore et dolore aliquyam erat, sed diam.</div>
+                    <div class="read">READ MORE</div>
                 </b-col>
             </b-row>
         </b-container>
@@ -84,117 +60,5 @@
 </script>
 
 <style scoped lang="scss">
-    #Blog {
-        padding-top: 90px;
-        padding-bottom: 80px;
 
-        .name_component {
-            font-size: 20px;
-            font-family: "Montserrat";
-            color: rgb(85, 85, 85);
-            font-weight: 600;
-            text-transform: uppercase;
-            line-height: 1.2;
-            text-align: center;
-            z-index: 416;
-        }
-
-        .descr_component {
-            font-size: 15.5px;
-            font-family: "Roboto Slab";
-            color: rgb(85, 85, 85);
-            letter-spacing: .28px;
-            line-height: 1.5;
-            text-align: center;
-            margin-top: 25.25px;
-            padding-bottom: 25px;
-        }
-
-        .underline {
-            background-color: rgb(85, 85, 85);
-            margin-top: 26px;
-            display: block;
-            margin-right: auto;
-            margin-left: auto;
-            width: 50px;
-            height: 2px;
-
-        }
-        .text {
-            font-size: 13.5px;
-            font-family: "Roboto Slab";
-            color: rgb(85, 85, 85);
-            letter-spacing: .275px;
-            line-height: 1.8;
-            text-align:left;
-            margin-top: 25.25px;
-        }
-        .blog{
-            padding-bottom: 26.5px;
-        }
-        .part_1,.part_2{padding-left: 30px !important;}
-        .part_2 .text{
-            padding: 0 12px 0 2px;
-
-            margin-top: 18px;
-        }
-        .date {
-            padding: 16px;
-            border: 2px solid rgb(119, 119, 119);
-            height: 75px;
-            width: 75px;
-            font-size: 24px;
-            font-family: "Montserrat";
-            color: rgb(119, 119, 119);
-            font-weight: bold;
-            text-transform: uppercase;
-            line-height: 0.833;
-            text-align: center;
-        }
-        .month{
-            font-size: 15px;
-            padding-top: 10px;
-        }
-        .name_post{
-            font-size: 16px;
-            font-family: "Montserrat";
-            color: rgb(85, 85, 85);
-            text-transform: uppercase;
-            line-height: 1.5;
-            text-align: left;
-            padding-top: 1.4%;
-            letter-spacing: -.18px;
-            padding-left: .5%;
-            font-weight: 600;
-        }
-        .blog:nth-child(4) .name_post{
-            padding-top: 1.5% !important;
-        }
-        .col .col{
-            padding: 0 !important;
-        }
-
-        .read {
-            padding-top: 21.5px;
-            font-size: 13.25px;
-            letter-spacing: .75px;
-            font-family: "Roboto Slab";
-            color: rgb(119, 119, 119);
-            font-weight: bold;
-            text-transform: uppercase;
-            line-height: 1.846;
-        }
-        svg{
-            margin-left: 15px;
-            margin-bottom: 3px;
-
-        }
-
-
-    }
-    @media (min-width: 1200px){
-        .container, .container-sm, .container-md, .container-lg, .container-xl {
-            max-width: 1200px !important;
-        }
-    }
 </style>
--- src/components/Call.vue
+++ src/components/Call.vue
@@ -16,37 +16,5 @@
 </script>
 
 <style scoped lang="scss">
-    #Call{
-        background: url("../../public/Call.png") 100% 100% no-repeat;
-     background-size:100% 100%;
 
-        #question{
-            font-size: 29.5px;
-            font-family: 'Roboto Slab', serif;
-            color: rgb(255, 255, 255);
-            font-weight: 400;
-            line-height: 1.48;
-            text-align: center;
-            letter-spacing: .28px;
-            z-index: 163;
-            padding-top: 90px;
-            padding-bottom: 50px;
-        }
-        #Get_Started {
-            font-size: 18px;
-            font-family: 'Roboto Slab', serif;
-            padding-bottom: 111px;
-            padding-left: 5px;
-            letter-spacing: .28px;
-            color: rgb(255, 255, 255);
-            text-transform: uppercase;
-            line-height: 1.5;
-            font-weight: 500;
-            width: 1900px;
-            margin-top: 5px;
-            text-align: center;
-            z-index: 428;
-        }
-
-    }
 </style>
