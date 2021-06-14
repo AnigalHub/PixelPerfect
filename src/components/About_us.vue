247c8da26f34756fe7d0d4c2a2f7ebbcf48272a4
AnigalHub <anigal1997@gmail.com>
2021-06-14 16:28:04 +0300

    вывод одинаковых боков через flex-container

--- src/components/About_us.vue
+++ src/components/About_us.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="About">
         <b-container>
-            <div class="name_component">ABOUT US</div>
+            <div class="name_component">About us</div>
             <svg class="underline" xmlns="http://www.w3.org/2000/svg">
                 <path fill="rgb(85, 85, 85)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999"/>
             </svg>
@@ -11,13 +11,13 @@
                     {{text.part}}
                 </b-col>
             </b-row>
-            <b-row class="imgs">
-                <b-col v-for="(img,index) in Imgs" :key="index">
+            <div class="flex-container">
+                <div v-for="(img,index) in Imgs" :key="index">
                     <img :src="img.img_src" :alt="img.img_alt">
                     <div class="text">{{img.text}}</div>
                     <div class="who">{{img.who}}</div>
-                </b-col>
-            </b-row>
+                </div>
+            </div>
         </b-container>
     </div>
 </template>
@@ -32,10 +32,10 @@
                     {part:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sediam nonumy eirtempor invidunt ut labore et dolore aliquyam erat, sed diam voluptua. At vero eos eaccusam et justo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor et dolore aliguyam erat. Loreipsum dolor sit amet. Lorem ipsum dolor et.',},
                 ],
                 Imgs:[
-                    {img_src:"./14.png",img_alt:"",text:'MD. KHALIL UDDIN',who:'Head of Ideas'},
-                    {img_src:"./13.png",img_alt:"",text:'RUBEL MIAH',who:'Lead WordPress Developer'},
-                    {img_src:"./12.png",img_alt:"",text:'SHAMIM MIA',who:'Sr. Web Developer'},
-                    {img_src:"./11.png",img_alt:"",text:'JOHN DOE',who:'Front-end Developer'},
+                    {img_src:"./About/MD._KHALIL_UDDIN.png",img_alt:"MD._KHALIL_UDDIN",text:'MD. KHALIL UDDIN',who:'Head of Ideas'},
+                    {img_src:"./About/RUBEL_MIAH.png",img_alt:"RUBEL_MIAH",text:'RUBEL MIAH',who:'Lead WordPress Developer'},
+                    {img_src:"./About/SHAMIM_MIA.png",img_alt:"SHAMIM_MIA",text:'SHAMIM MIA',who:'Sr. Web Developer'},
+                    {img_src:"./About/JOHN_DOE.png",img_alt:"JOHN_DOE",text:'JOHN DOE',who:'Front-end Developer'},
                 ]
             }
         }
