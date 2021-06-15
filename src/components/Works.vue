7f4dbbf81a0c08870fff77d8845efc293df021d4
AnigalHub <anigal1997@gmail.com>
2021-06-15 04:02:21 +0300

    изменено название, добавлены пути до картинок и alt

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="Works">
         <b-container>
-        <div class="name_component">LATEST WORKS</div>
-        <svg class="underline" xmlns="http://www.w3.org/2000/svg" width="50px" height="2px">
-            <path fill-rule="evenodd"  fill="rgb(85, 85, 85)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999 "/>
+        <div class="name_component">Latest works</div>
+        <svg class="underline" xmlns="http://www.w3.org/2000/svg" >
+            <path fill="rgb(85, 85, 85)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999 "/>
         </svg>
         <div class="nav_component">
             <b-navbar-nav align="center">
-                <b-nav-item>All</b-nav-item>
+                <b-nav-item id="first">All</b-nav-item>
                 <b-nav-item>Branding</b-nav-item>
                 <b-nav-item>Design</b-nav-item>
                 <b-nav-item>Development</b-nav-item>
@@ -26,17 +26,16 @@
         name: "Works",
         data(){
             return{
-                publicPath: process.env.BASE_URL,
                Works:[
-                    {src: "./1.png", alt: "",},
-                    {src: "./2.png", alt: "",},
-                    {src: "./3.png", alt: "",},
-                    {src: "./4.png", alt: "",},
-                    {src: "./5.png", alt: "",},
-                    {src: "./6.png", alt: "",},
-                    {src: "./7.png", alt: "",},
-                    {src: "./8.png", alt: "",},
-                    {src: "./9.png", alt: "",},
+                    {src: "./Works/Hair_Dresser.png", alt: "Hair_Dresser",},
+                    {src: "./Works/Bird.png", alt: "Bird",},
+                    {src: "./Works/Work_Caption.png", alt: "Work_Caption",},
+                    {src: "./Works/Camera.png", alt: "Camera",},
+                    {src: "./Works/Bicycle.png", alt: "Bicycle",},
+                    {src: "./Works/Single_Work.png", alt: "Single_Work",},
+                    {src: "./Works/Workplace.png", alt: "Workplace",},
+                    {src: "./Works/Stationery.png", alt: "Stationery",},
+                    {src: "./Works/Events_Entertainment.png", alt: "Events_Entertainment",},
                 ],
             }
         }
