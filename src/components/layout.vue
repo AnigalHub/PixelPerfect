4ae0886824d8ce0f463dbd6834352c54931e1c88
AnigalHub <anigal1997@gmail.com>
2021-06-16 12:19:53 +0300

    добавлен компонент отвечающий за шаблон (начала почти всех страниц - название,svg,описание)

---
+++ src/components/layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="name_component">{{name_component}}</div>
+        <svg class="underline" xmlns="http://www.w3.org/2000/svg">
+            <path class="color_underline"  d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999"/>
+        </svg>
+        <p class="descr_component">{{descr_component}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout",
+        props:{
+            name_component: String,
+            descr_component: String,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
