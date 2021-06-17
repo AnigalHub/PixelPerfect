ed59531abc6744c581d9ba8fbbafff7c3939cacb
AnigalHub <anigal1997@gmail.com>
2021-06-17 17:06:42 +0300

    добавлен компонент шаблона

--- src/components/About_us.vue
+++ src/components/About_us.vue
@@ -1,15 +1,9 @@
 <template>
     <div id="About">
         <b-container>
-            <div class="name_component">About us</div>
-            <svg class="underline" xmlns="http://www.w3.org/2000/svg">
-                <path fill="rgb(85, 85, 85)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999"/>
-            </svg>
-            <p class="descr_component">Lorem ipsum dolor sit amet, consetetur sadipscing elitr amet</p>
+            <layout :name_component="name_component" :descr_component="descr_component"/>
             <b-row class="parts">
-                <b-col class="text" v-for="(text,index) in DescriptionText" :key="index">
-                    {{text.part}}
-                </b-col>
+                <b-col class="text" v-for="(text,index) in DescriptionText" :key="index">{{text.part}}</b-col>
             </b-row>
             <div class="flex-container">
                 <div v-for="(img,index) in Imgs" :key="index">
@@ -23,10 +17,14 @@
 </template>
 
 <script>
+    import Layout from "./layout";
     export default {
         name: "About_us",
+        components: {Layout},
         data(){
             return{
+                name_component:"About us",
+                descr_component:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr amet",
                 DescriptionText:[
                     {part:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sediam nonumy eirmod tempor invidunt ut labore et dolore aliquyam erat, sed diam voluptua. At vero eos et accusam et justo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor et dolore aliguyam erat. Lorem ipsum dolor sit amet. Lorem ipsum eat.',},
                     {part:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sediam nonumy eirtempor invidunt ut labore et dolore aliquyam erat, sed diam voluptua. At vero eos eaccusam et justo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor et dolore aliguyam erat. Loreipsum dolor sit amet. Lorem ipsum dolor et.',},
