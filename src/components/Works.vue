bbf6bc197b9fbb368cabef0993e2448157be444b
AnigalHub <anigal1997@gmail.com>
2021-06-20 17:58:09 +0300

    добавлен блок при наведении (название, тип, кнопка)

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,42 +1,54 @@
 <template>
     <div id="Works">
         <b-container>
-        <div class="name_component">Latest works</div>
-        <svg class="underline" xmlns="http://www.w3.org/2000/svg" >
-            <path fill="rgb(85, 85, 85)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999 "/>
-        </svg>
-        <div class="nav_component">
-            <b-navbar-nav align="center">
-                <b-nav-item id="first">All</b-nav-item>
-                <b-nav-item>Branding</b-nav-item>
-                <b-nav-item>Design</b-nav-item>
-                <b-nav-item>Development</b-nav-item>
-                <b-nav-item>Strategy</b-nav-item>
-            </b-navbar-nav>
-        </div>
-        <div class="flex-container">
-            <img v-for="(work, index) in Works" :key="index" :src="work.src" :alt="work.alt"/>
-        </div>
+            <layout :name_component="name_component"/>
+            <div class="nav_component">
+                <b-navbar-nav align="center">
+                    <b-nav-item id="first">All</b-nav-item>
+                    <b-nav-item>Branding</b-nav-item>
+                    <b-nav-item>Design</b-nav-item>
+                    <b-nav-item>Development</b-nav-item>
+                    <b-nav-item>Strategy</b-nav-item>
+                </b-navbar-nav>
+            </div>
+            <div class="flex-container">
+                <div v-for="(work, index) in Works" :key="index">
+                    <img :src="work.img_src" :alt="work.img_alt"/>
+                    <div class="work_hover">
+                        <div class="name_work">{{work.name}}</div>
+                        <div class="type_work">{{work.type}}</div>
+                        <b-button>
+                            <div class="view">View</div>
+                            <component :is="arrow"/>
+                        </b-button>
+                    </div>
+                </div>
+            </div>
         </b-container>
     </div>
 </template>
 
 <script>
+    import Layout from "./layout";
+    import arrowSVG from "./svg_works/arrow_svg";
     export default {
         name: "Works",
+        components: {Layout},
         data(){
             return{
+               name_component:"Latest works",
                Works:[
-                    {src: "./Works/Hair_Dresser.png", alt: "Hair_Dresser",},
-                    {src: "./Works/Bird.png", alt: "Bird",},
-                    {src: "./Works/Work_Caption.png", alt: "Work_Caption",},
-                    {src: "./Works/Camera.png", alt: "Camera",},
-                    {src: "./Works/Bicycle.png", alt: "Bicycle",},
-                    {src: "./Works/Single_Work.png", alt: "Single_Work",},
-                    {src: "./Works/Workplace.png", alt: "Workplace",},
-                    {src: "./Works/Stationery.png", alt: "Stationery",},
-                    {src: "./Works/Events_Entertainment.png", alt: "Events_Entertainment",},
+                    {img_src: "./Works/Hair_Dresser.png", img_alt: "Hair_Dresser",name:"Hair Dresser",type:"Branding"},
+                    {img_src: "./Works/Bird.png", img_alt: "Bird",name:"Bird",type:"Design"},
+                    {img_src: "./Works/Work_Caption.png", img_alt: "Work_Caption",name:"Work Caption",type:"Development"},
+                    {img_src: "./Works/Camera.png", img_alt: "Camera", name:"Camera",type:"Strategy"},
+                    {img_src: "./Works/Bicycle.png", img_alt: "Bicycle",name:"Bicycle",type:"Strategy"},
+                    {img_src: "./Works/Single_Work.png", img_alt: "Single_Work",name:"Single Work",type:"Development"},
+                    {img_src: "./Works/Workplace.png", img_alt: "Workplace",name:"Workplace",type:"Development"},
+                    {img_src: "./Works/Stationery.png", img_alt: "Stationery",name:"Stationery",type:"Design"},
+                    {img_src: "./Works/Events_Entertainment.png", img_alt: "Design",name:"Design",type:"Branding"},
                 ],
+                arrow:arrowSVG,
             }
         }
     }
