dfe5e216d3de01ff35b15ac8a99017980360ec00
AnigalHub <anigal1997@gmail.com>
2021-06-23 18:33:47 +0300

    содержимое обернуто в блок со своим красным затемнением

--- src/components/Call.vue
+++ src/components/Call.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="Call">
-        <div id="question">
-            Do you like OUR WORK so far?
-            <br>
-            Let's talk about YOUR PROJECT!
+        <div id="background">
+            <div id="question">
+                Do you like OUR WORK so far?
+                <br>
+                Let's talk about YOUR PROJECT!
+            </div>
+            <div id="Get_Started">Get in touch</div>
         </div>
-        <div id="Get_Started">Get In Touch</div>
     </div>
 </template>
 
