c8a94c356bd758075f3a2515d12cabf8c6e85f69
AnigalHub <anigal1997@gmail.com>
2021-06-24 16:47:00 +0300

    вывод картинок через цикл, импорт points, удалены стили

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -1,76 +1,45 @@
 <template>
     <div id="Clients">
         <b-container>
-           <b-row>
-               <b-col>
-                   <img src="../../public/clients5.png">
-               </b-col>
-               <b-col>
-                   <img src="../../public/clients4.png">
-               </b-col>
-               <b-col>
-                   <img src="../../public/clients1.png">
-               </b-col>
-               <b-col>
-                   <img src="../../public/clients3.png">
-               </b-col>
-               <b-col>
-                   <img src="../../public/clients2.png">
-               </b-col>
-           </b-row>
-            <div class="text">
-                Hvaing placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-                <br>
-                Temporibus autem quibusdam et aut officiis debitis aut rerum"
+           <div class="flex-container clients">
+               <div v-for="(client,index) in Clients" :key="index">
+                   <img :src="client.img_src" :alt="client.img_alt">
+               </div>
+           </div>
+           <div class="phrase">
+               Hvaing placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+               <br>
+               Temporibus autem quibusdam et aut officiis debitis aut rerum"
+           </div>
+           <div class="author">John Doe, Google Inc.</div>
+            <div class="points flex-container">
+                <div v-for="(value,index) in Points" :key="index">
+                    <component :is="value.svg"/>
+                </div>
             </div>
-            <div class="author">John Doe,Google Inc.</div>
         </b-container>
     </div>
 </template>
 
 <script>
+    import PointSVG from "./svg_clients/svg_point";
     export default {
-        name: "Clients"
+        name: "Clients",
+        data(){
+            return{
+                Points:[{svg:PointSVG}, {svg:PointSVG}, {svg:PointSVG},],
+                Clients: [
+                    {img_src: "./Clients/Deorham.png",img_alt: "Deorham",},
+                    {img_src: "./Clients/Ratings.png", img_alt: "Ratings",},
+                    {img_src: "./Clients/Malk_Media.png", img_alt: "Malk_Media",},
+                    {img_src: "./Clients/Bcause.png", img_alt: "Bcause",},
+                    {img_src: "./Clients/Wompify.png",img_alt: "Wompify",},
+                ]
+            }
+        }
     }
 </script>
 
 <style scoped>
-    #Clients{
-        padding-top: 105px;
-        padding-bottom: 150px;
-        background: #f8f8f8;
-    }
-    .row{
-        padding: 0 90px !important;
-    }
-    .col{
-        padding: 0 0 0 30px !important;
-    }
-    .col:nth-child(2){
-        margin-left: 1.3%;
-        margin-top: -1%;
-    }
-    .col:nth-child(3){
-        padding-left: 15px !important;
-    }
-    .col:nth-child(4){
-        padding-left: 15px !important;
-    }
-    .text {
-        margin-top: 4.8%;
-        font-size: 18px;
-        font-family: "Roboto Slab";
-        color: rgb(85, 85, 85);
-        line-height: 1.667;
-        text-align: center;
-    }
-    .author{
-        margin-top: 2.4%;
-        font-size: 21px;
-        font-family: "Roboto Slab";
-        color: rgb(85, 85, 85);
-        line-height: 1.667;
-        text-align: center;
-    }
 
 </style>
