89676993c439ea76ed90d9aade81061193135aef
AnigalHub <anigal1997@gmail.com>
2021-06-27 19:35:33 +0300

    изменен регистр фраз

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,9 @@
         <div id="background">
             <Navbar/>
             <b-container>
-                <p id="Welcome_to_STARTUP">Welcome to StartUp</p>
-                <div id="Your_Favourite_Creative_Age">Your Favourite Creative Agency Template</div>
-                <div id="Get_Started">Get Started</div>
+                <p id="Welcome_to_STARTUP">Welcome to startup</p>
+                <div id="Your_Favourite_Creative_Age">Your favourite creative agency template</div>
+                <div id="Get_Started">Get started</div>
             </b-container>
         </div>
     </div>
