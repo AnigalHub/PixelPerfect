be0a8a16bbf08abc41ead8a86a6665076eb7c83b
AnigalHub <anigal1997@gmail.com>
2021-06-28 16:14:16 +0300

    добавлен компонент с шаблоном

--- src/components/myfooter.vue
+++ src/components/myfooter.vue
@@ -6,18 +6,18 @@
                     <component :is="value.svg"/>
                 </div>
             </div>
-            <svg class="underline" xmlns="http://www.w3.org/2000/svg" >
-                <path fill="rgb(255, 255, 255)" d="M0.000,1.999 L0.000,-0.000 L50.000,-0.000 L50.000,1.999 L0.000,1.999 "/>
-            </svg>
+            <layout/>
             <div id="designer">© 2015 Startup, designer by shapedtheme</div>
         </b-container>
     </div>
 </template>
 
 <script>
-    import TwitterSVG from '../components/svg_twitter';
+    import TwitterSVG from './svg_footer/svg_twitter';
+    import Layout from "./layout";
     export default {
-        name: "footer",
+        name: "myfooter",
+        components: {Layout},
         data(){
             return{
                Svg:[{svg:TwitterSVG}, {svg:TwitterSVG}, {svg:TwitterSVG}, {svg:TwitterSVG},]
